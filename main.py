b4791e4fe94f64ad77c46332c39d8246230ed372
scorpiotea <112612010+scorpiotea@users.noreply.github.com>
2023-06-19 19:21:40 +0200

    Added a comment

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ directory = input("Enter the name of the directory you want to create: ")
 
 os.mkdir(directory)
 
+#function to download youtube video
 def Download(link):
     youtubeObject = YouTube(link)
     youtubeObject = youtubeObject.streams.get_highest_resolution()
