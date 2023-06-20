0199b03eae3724be90b52f37d0d97ffa5281b7ba
scorpiotea <112612010+scorpiotea@users.noreply.github.com>
2023-06-20 19:44:08 +0200

    Changes to functions

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ from pytube import YouTube
 args = sys.argv
 
 #function to get data from text file
-def GetFileData():
-    modifiedLink = []
-    file = open(fileName, 'r')
+def GetFileData(arg):
+    modifiedLine = []
+    file = open(arg, 'r')
     read = file.readlines()
     file.close()
 
-    for link in read:
-        if link[-1] == '\n':
-            modifiedLink.append(link[:-1])
+    for line in read:
+        if line[-1] == '\n':
+            modifiedLine.append(line[:-1])
         else:
-            modifiedLink.append(link)
-    print(modifiedLink)
-    Download(modifiedLink)
+            modifiedLine.append(line)
+    
+    for link in modifiedLine:
+        Download(link)
 
 #function to download youtube video
-def Download(videoLink):
-    for link in videoLink:
+def Download(link):
         youtubeObject = YouTube(link)
         youtubeObject = youtubeObject.streams.get_highest_resolution()
         try:
@@ -30,12 +30,12 @@ def Download(videoLink):
             print('An error has occurred')
         print('Download is completed successfully')
 
-#checking if provided arguments are files
-if os.path.isfile(args[1]):
-    fileName = args[1]
-    GetFileData()
-else:
-    args = args[1:]
-    Download(args)
+#function to check if provided arguments are files
+def CheckArgs():
+    for arg in args[1:]:
+        if os.path.isfile(arg):
+            GetFileData(arg)
+        else:
+            Download(arg)
 
-
+CheckArgs()
