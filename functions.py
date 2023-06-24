7f16710a82542ee0dc1cdff827fef981c7a14998
scorpiotea <112612010+scorpiotea@users.noreply.github.com>
2023-06-24 17:12:37 +0200

    Functions put into module

---
+++ functions.py
@@ -0,0 +1,40 @@
+import os
+from pytube import YouTube
+
+#function to get data from text file
+def get_file_data(arg):
+    modified_line = []
+    file = open(arg, 'r')
+    read = file.readlines()
+    file.close()
+
+    for line in read:
+        if line[-1] == '\n':
+            modified_line.append(line[:-1])
+        else:
+            modified_line.append(line)
+    
+    for link in modified_line:
+        download(link)
+
+#function to download youtube video
+def download(link):
+        youtube_object = YouTube(link)
+        youtube_object = youtube_object.streams.get_highest_resolution()
+        try:
+            youtube_object.download()
+        except:
+            print('An error has occurred')
+        print('Download is completed successfully')
+
+#function to check if provided arguments are files
+def check_arguments(args):
+    if len(args) > 1 : 
+        for arg in args[1:]:
+            if os.path.isfile(arg):
+                get_file_data(arg)
+            else:
+                download(arg)
+    else:
+        print('No arguments provided')
+        exit()
